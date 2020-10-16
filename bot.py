1a665282e58d70bf8d6c0bc32f6ee5f50c625d2e
IlyaAlejchik <ilya.alejchik@outlook.com>
2020-10-16 21:14:41 +0300

    were added the poll

--- bot.py
+++ bot.py
@@ -3,13 +3,15 @@ import telebot
 import random
 import re
 
-username = ""
-
 file_id = "CAACAgIAAxkBAAOEX4l-hyLbIdCKcM7FPJCXNwKBeYAAAooBAAIQIQIQcMU5aupZY9MbBA"
 
+
+question = ["Вопрос"]
+answers = ["Ответ1","Ответ2", "Ответ3"]
 arrMessage = ["Привет", "Ку", "Хай"]
-arrText = ["Ку", "Да я тут", "Что нада",
-           "Напише мне привет хотя-б", "Я тебя не понимаю. Напиши /help."]
+arrText = ["Ку", "Да я тут", "Что нада","Напише мне привет хотя-б", "Я тебя не понимаю. Напиши /help."]
+
+
 
 stopText = ["Пока", "До встречи", "До свидания"]
 
@@ -26,12 +28,20 @@ def stop_bott(message):
     bot.send_message(message.chat.id, "Отключаюсь")
     bot.stop_bot()
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(
+        message.chat.id, "Комманды бота \n /stop - отключение бота \n ")
+
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.send_message(
         message.chat.id, "Комманды бота \n /stop - отключение бота \n ")
 
+@bot.message_handler(commands=['poll'])
+def sendpooling(message):
+    bot.send_poll(message.chat.id, question, answers)
 
 @bot.message_handler(content_types=['text'])
 def send_addfriend(message):
@@ -52,6 +62,7 @@ def get_username(message):
     bot.send_message(message.chat.id, "Очень приятно " +
                      message.text + " а меня Бот")
     bot.send_sticker(message.chat.id, file_id)
+
 
 
 if __name__ == "__main__":
