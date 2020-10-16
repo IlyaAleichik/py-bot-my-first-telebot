293d5981792275ecc6fd9686c92569ed6b365bf3
Илья <Ilya.alejchik@outlook.com>
2020-10-16 19:16:45 +0300

    pyTelegramBotApi my first bot, learning language python

---
+++ bot.py
@@ -0,0 +1,58 @@
+import config
+import telebot
+import random
+import re
+
+username = ""
+
+file_id = "CAACAgIAAxkBAAOEX4l-hyLbIdCKcM7FPJCXNwKBeYAAAooBAAIQIQIQcMU5aupZY9MbBA"
+
+arrMessage = ["Привет", "Ку", "Хай"]
+arrText = ["Ку", "Да я тут", "Что нада",
+           "Напише мне привет хотя-б", "Я тебя не понимаю. Напиши /help."]
+
+stopText = ["Пока", "До встречи", "До свидания"]
+
+command = ["отключение", "stop"]
+
+bot = telebot.TeleBot(config.token)
+
+keyboard = telebot.types.ReplyKeyboardMarkup()
+keyboard.row('Привет', 'Пока', 'Ку', 'Познакомимся')
+
+
+@bot.message_handler(commands=['stop'])
+def stop_bott(message):
+    bot.send_message(message.chat.id, "Отключаюсь")
+    bot.stop_bot()
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(
+        message.chat.id, "Комманды бота \n /stop - отключение бота \n ")
+
+
+@bot.message_handler(content_types=['text'])
+def send_addfriend(message):
+    if message.text == "Познакомимся":
+        bot.send_message(message.chat.id, " Давай знакомиться. Как тебя зовут")
+        bot.register_next_step_handler(message, get_username)
+    elif message.text in arrMessage:
+        bot.send_message(message.chat.id, "Привет", reply_markup=keyboard)
+    elif message.text in stopText:
+        bot.send_message(message.chat.id, stopText[random.randrange(0, 3)])
+    else:
+        bot.send_message(message.chat.id, arrText[random.randrange(0, 4)])
+
+
+@bot.message_handler(content_types=['text'])
+def get_username(message):
+
+    bot.send_message(message.chat.id, "Очень приятно " +
+                     message.text + " а меня Бот")
+    bot.send_sticker(message.chat.id, file_id)
+
+
+if __name__ == "__main__":
+    bot.polling()
---
+++ config.py
@@ -0,0 +1,1 @@
+token = "1002839138:AAFpdrsBgQAPRUykvzF-s7-5ZShjrr1u2F4"
