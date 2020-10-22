c831685e21bfcd27174c023b518259e37ec08f16
Илья <Ilya.alejchik@outlook.com>
2020-10-22 16:52:43 +0300

    added button of the url link

--- bot.py
+++ bot.py
@@ -3,14 +3,16 @@ import telebot
 import random
 import re
 
+from telebot import types
+
 file_id = "CAACAgIAAxkBAAOEX4l-hyLbIdCKcM7FPJCXNwKBeYAAAooBAAIQIQIQcMU5aupZY9MbBA"
 
 
 question = ["Вопрос"]
-answers = ["Ответ1","Ответ2", "Ответ3"]
+answers = ["Ответ1", "Ответ2", "Ответ3"]
 arrMessage = ["Привет", "Ку", "Хай"]
-arrText = ["Ку", "Да я тут", "Что нада","Напише мне привет хотя-б", "Я тебя не понимаю. Напиши /help."]
-
+arrText = ["Ку", "Да я тут", "Что нада",
+           "Напишите мне привет хотя-б", "Я тебя не понимаю. Напиши /help."]
 
 
 stopText = ["Пока", "До встречи", "До свидания"]
@@ -28,20 +30,17 @@ def stop_bott(message):
     bot.send_message(message.chat.id, "Отключаюсь")
     bot.stop_bot()
 
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.send_message(
-        message.chat.id, "Комманды бота \n /stop - отключение бота \n ")
-
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.send_message(
         message.chat.id, "Комманды бота \n /stop - отключение бота \n ")
+
 
 @bot.message_handler(commands=['poll'])
 def sendpooling(message):
     bot.send_poll(message.chat.id, question, answers)
+
 
 @bot.message_handler(content_types=['text'])
 def send_addfriend(message):
@@ -49,7 +48,10 @@ def send_addfriend(message):
         bot.send_message(message.chat.id, " Давай знакомиться. Как тебя зовут")
         bot.register_next_step_handler(message, get_username)
     elif message.text in arrMessage:
-        bot.send_message(message.chat.id, "Привет", reply_markup=keyboard)
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://yandex.by")
+        keyboard.add(url_button)
+        bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
     elif message.text in stopText:
         bot.send_message(message.chat.id, stopText[random.randrange(0, 3)])
     else:
@@ -58,12 +60,9 @@ def send_addfriend(message):
 
 @bot.message_handler(content_types=['text'])
 def get_username(message):
-
     bot.send_message(message.chat.id, "Очень приятно " +
                      message.text + " а меня Бот")
     bot.send_sticker(message.chat.id, file_id)
-
-
-
+    
 if __name__ == "__main__":
     bot.polling()
